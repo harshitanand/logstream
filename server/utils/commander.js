9c27d0c9e2c93aaeb5d6c3cf1f714fe55d6a552e
lucifer <harshitanand94@gmail.com>
2020-03-23 19:14:26 +0530

    execute permission changes

--- server/utils/commander.js
+++ server/utils/commander.js
@@ -9,10 +9,10 @@ cmd
   .version(require('../../package.json').version)
   .usage('[options] file')
   .helpOption('--help')
-  .option('-h, --hostname <host>', 'hostname for app default listen IP, default 0.0.0.0', String, '0.0.0.0')
+  .option('-h, --host <host>', 'hostname for app default listen IP, default 0.0.0.0', String, '0.0.0.0')
   .option('-p, --port <port>', 'port for app default listen socket, default 50051', Number, 50051)
-  .option('-l, --lineslimit <lines>', 'number on lines stored in browser, default 2000', Number, 2000)
-  .option('-U, --username <username>', 'Basic Auth username, option works only along with -P option', String, false)
+  .option('-l, --lines <lines>', 'number on lines stored in browser, default 2000', Number, 2000)
+  .option('-U, --user <username>', 'Basic Auth username, option works only along with -P option', String, false)
   .option('-P, --password <password>', 'Basic Auth password, option works only along with -U option', String, false)
   .option(
     '-k, --key <key.pem>',
@@ -21,7 +21,7 @@ cmd
     false
   )
   .option(
-    '-c, --cert <cert.pem>',
+    '-c, --certificate <cert.pem>',
     'Certificate for HTTPS connections on app, option works only along with -k option',
     String,
     false
